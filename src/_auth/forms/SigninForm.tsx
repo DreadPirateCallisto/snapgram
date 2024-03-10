6a6fb6a1e293909a2d5115af0d45d74a158eeea0
Mpho Owen <139195377+DreadPirateCallisto@users.noreply.github.com>
2024-03-10 17:15:16 +0200

    try 6

--- src/_auth/forms/SigninForm.tsx
+++ src/_auth/forms/SigninForm.tsx
@@ -23,7 +23,7 @@ import { useUserContext } from "@/context/AuthContext"
 
 const SigninForm = () => {
   const { toast } = useToast()
-  const { checkAuthUser, isLoading: isUserLoading } = useUserContext();
+  const { checkAuthUser, isPending: isUserLoading } = useUserContext();
   const navigate = useNavigate();
 
   const { mutateAsync: signInAccount } = useSignInAccount();
