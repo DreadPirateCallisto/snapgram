e6eed97a8e20895c752c8ed47129e36eeddb40d6
Mpho Owen <139195377+DreadPirateCallisto@users.noreply.github.com>
2024-03-10 04:08:44 +0200

    try 4

--- src/_root/pages/PostDetails.tsx
+++ src/_root/pages/PostDetails.tsx
@@ -1,22 +1,25 @@
-import GridPostList from "@/components/shared/GridPostList";
-import Loader from "@/components/shared/Loader";
-import PostStats from "@/components/shared/PostStats";
-import { Button } from "@/components/ui/button";
+import { useParams, Link, useNavigate } from "react-router-dom";
+
+
+
+
+import { multiFormatDateString } from "@/lib/utils";
 import { useUserContext } from "@/context/AuthContext";
 import { useDeletePost, useGetPostById, useGetUserPosts } from "@/lib/react-query/queriesAndMutations";
-import { multiFormatDateString } from "@/lib/utils";
-import { Link, useNavigate, useParams } from "react-router-dom"
+import { Button } from "@/components/ui/button";
+import Loader from "@/components/shared/Loader";
+import PostStats from "@/components/shared/PostStats";
+import GridPostList from "@/components/shared/GridPostList";
 
-const postDetails = () => {
+const PostDetails = () => {
   const navigate = useNavigate();
   const { id } = useParams();
-  const { data: post, isPending } = useGetPostById(id || '');
   const { user } = useUserContext();
 
-  const { data: userPosts, isLoading: isUserPostLoading } = useGetUserPosts(
+  const { data: post, isPending } = useGetPostById(id || '');
+  const { data: userPosts, isPending: isUserPostLoading } = useGetUserPosts(
     post?.creator.$id
   );
-
   const { mutate: deletePost } = useDeletePost();
 
   const relatedPosts = userPosts?.documents.filter(
@@ -24,34 +27,51 @@ const postDetails = () => {
   );
 
   const handleDeletePost = () => {
-
     deletePost({ postId: id || '', imageId: post?.imageId });
     navigate(-1);
-  }
+  };
 
   return (
     <div className="post_details-container">
-      {isPending ? <Loader /> : (
+      <div className="hidden md:flex max-w-5xl w-full">
+        <Button
+          onClick={() => navigate(-1)}
+          variant="ghost"
+          className="shad-button_ghost">
+          <img
+            src={"/assets/icons/back.svg"}
+            alt="back"
+            width={24}
+            height={24}
+          />
+          <p className="small-medium lg:base-medium">Back</p>
+        </Button>
+      </div>
+
+      {isPending || !post ? (
+        <Loader />
+      ) : (
         <div className="post_details-card">
-          <img 
-            src={post?.imageUrl} 
-            alt="post" 
+          <img
+            src={post?.imageUrl}
+            alt="creator"
             className="post_details-img"
           />
+
           <div className="post_details-info">
             <div className="flex-between w-full">
-              <Link to={`/profile/${post?.creator.$id}`} className="flex items-center gap-3">
+              <Link
+                to={`/profile/${post?.creator.$id}`}
+                className="flex items-center gap-3">
                 <img
                   src={
-                    post?.creator?.imageUrl ||
+                    post?.creator.imageUrl ||
                     "/assets/icons/profile-placeholder.svg"
                   }
                   alt="creator"
-                  className="w-8 h-8 rounded-full lg:w-12 lg:h-12"
+                  className="w-8 h-8 lg:w-12 lg:h-12 rounded-full"
                 />
-              
-
-                <div className="flex flex-col">
+                <div className="flex gap-1 flex-col">
                   <p className="base-medium lg:body-bold text-light-1">
                     {post?.creator.name}
                   </p>
@@ -68,27 +88,30 @@ const postDetails = () => {
               </Link>
 
               <div className="flex-center gap-4">
-                  <Link to={`/update-post/${post?.$id}`} className={`${user.id !== post?.creator.$id && 'hidden'}`}>
-                    <img 
-                      src="/assets/icons/edit.svg" 
-                      alt="edit" 
-                      width={24}
-                      height={24}
-                    />
-                  </Link>
+                <Link
+                  to={`/update-post/${post?.$id}`}
+                  className={`${user.id !== post?.creator.$id && "hidden"}`}>
+                  <img
+                    src={"/assets/icons/edit.svg"}
+                    alt="edit"
+                    width={24}
+                    height={24}
+                  />
+                </Link>
 
-                  <Button 
-                    onClick={handleDeletePost}
-                    variant='ghost'
-                    className={`ghost_details-delete_btn ${user.id !== post?.creator.$id && 'hidden'}`}
-                    >
-                    <img 
-                      src="/assets/icons/delete.svg" 
-                      alt="delete" 
-                      width={24}
-                      height={24}
-                    />
-                  </Button>
+                <Button
+                  onClick={handleDeletePost}
+                  variant="ghost"
+                  className={`ost_details-delete_btn ${
+                    user.id !== post?.creator.$id && "hidden"
+                  }`}>
+                  <img
+                    src={"/assets/icons/delete.svg"}
+                    alt="delete"
+                    width={24}
+                    height={24}
+                  />
+                </Button>
               </div>
             </div>
 
@@ -96,19 +119,20 @@ const postDetails = () => {
 
             <div className="flex flex-col flex-1 w-full small-medium lg:base-regular">
               <p>{post?.caption}</p>
-                <ul className="flex gap-1 mt-2">
-                  {post?.tags.map((tag: string, index: string) => (
-                    <li key={`${tag}${index}`} className="text-light-3 small-regular">
-                      #{tag}
-                    </li>
-                  ))}
-                </ul>
+              <ul className="flex gap-1 mt-2">
+                {post?.tags.map((tag: string, index: string) => (
+                  <li
+                    key={`${tag}${index}`}
+                    className="text-light-3 small-regular">
+                    #{tag}
+                  </li>
+                ))}
+              </ul>
             </div>
 
             <div className="w-full">
-              <PostStats post={post} userId={user.id}/>
+              <PostStats post={post} userId={user.id} />
             </div>
-
           </div>
         </div>
       )}
@@ -126,7 +150,7 @@ const postDetails = () => {
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default postDetails;
+export default PostDetails;
